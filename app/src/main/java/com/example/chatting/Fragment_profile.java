b16acc4bf117da54dc84a5bb42be5208b9af3457
Choi wonkyoung <cwk6601@naver.com>
2021-01-04 19:35:12 +0900

    * 프로필 구조만 일단 구현   이후 추가 예정
    설정 구현 예정

--- app/src/main/java/com/example/chatting/Fragment_profile.java
+++ app/src/main/java/com/example/chatting/Fragment_profile.java
@@ -1,13 +1,29 @@
 package com.example.chatting;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +31,12 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class Fragment_profile extends Fragment {
+    private static final String TAG = "Fragment_profile";
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    List<String> list;
+
+    private TextView name, nickname, phoneNumber, birth;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -62,9 +84,43 @@ public class Fragment_profile extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Fragment_chatting.start = 0;
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        name = v.findViewById(R.id.name);
+        nickname = v.findViewById(R.id.nickname);
+        phoneNumber = v.findViewById(R.id.phoneNumber);
+        birth = v.findViewById(R.id.birth);
+
+        DocumentReference docRef = db.collection("user").document(user.getUid());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    assert document != null;
+
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                        Map<String, Object> map = document.getData();
+
+                        assert map != null;
+                        name.setText(map.get("name").toString());
+                        nickname.setText("닉네임 : " + map.get("nickname").toString());
+                        phoneNumber.setText("휴대폰 번호 : " + map.get("phone").toString());
+                        birth.setText("생일 : " + map.get("birth").toString());
+
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        return v;
 
     }
 }
